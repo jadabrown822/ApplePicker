5a0a8bee01262493daf70037b17700d3098406d1
jadabrown822 <jadabrown822@gmail.com>
2025-09-18 15:10:56 -0500

    Update RoundNumber.cs
    is timer for round change, will mess with length

--- Assets/RoundNumber.cs
+++ Assets/RoundNumber.cs
@@ -1,9 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class RoundNumber : MonoBehaviour
 {
+    public TextMeshProUGUI roundText;
+
+    private int currentLevel = 1;
+
+    public float timeToNextRound = 5.0f;
+
+
+    private void start() 
+    {
+        UpdateRoundText();
+        StartCoroutine(IncreaseRoundOverTime());
+    }
+
+
+    private void UpdateRoundText() 
+    {
+        roundText.text = "Round " + currentRound.ToString();
+    }
+
+
+    private IEnumerator IncreaseRondOverTime()
+    {
+        while (currentRound < 4)
+        {
+            yield return new WaitForSecond(timeToNextRound);
+
+            currentRound++;
+            UpdateRoundText();
+        }
+    }
+    
+    /*
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +48,5 @@ public class RoundNumber : MonoBehaviour
     {
         
     }
+    */
 }
